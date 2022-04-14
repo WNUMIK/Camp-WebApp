894cd2e4b895516deb2aa170aac3b5c46553d557
wnumik <k1wsatz@wnumiks-MacBook-Pro.local>
2022-04-14 21:31:24 +0200

    reservation date calednar

--- campings/views.py
+++ campings/views.py
@@ -1,3 +1,4 @@
+from django import forms
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.shortcuts import render
 
@@ -42,6 +43,16 @@ class ReserveCampingView(LoginRequiredMixin, PermissionRequiredMixin, CreateView
     login_url = reverse_lazy('users:login')
     raise_exception = True
     success_url = reverse_lazy('home:home')
+
+    def get_form(self, form_class=None):
+        form = super().get_form(form_class)
+        form.fields['check_in'].widget = forms.TextInput(
+            attrs={'type': 'date'}
+        )
+        form.fields['check_out'].widget = forms.TextInput(
+            attrs={'type': 'date'}
+        )
+        return form
 
     def form_valid(self, form):
         form.instance.regular = self.request.user
