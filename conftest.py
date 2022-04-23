3eaab75eaa17e13f0ee3ab870ec28a6b16777d7f
wnumik <k1wsatz@wnumiks-MacBook-Pro.local>
2022-04-23 16:06:20 +0200

    fixtures & camping views tests

--- conftest.py
+++ conftest.py
@@ -1,7 +1,22 @@
 import pytest
 
+from campings.models import Place, Camping
+
 
 @pytest.fixture(scope='function')
 def user(db, django_user_model):
     """User instance from default django user model"""
     yield django_user_model.objects.create_user(email='a@a.pl', fullname='Ala', password='testPass123')
+
+
+@pytest.fixture(scope='function')
+def place(user):
+    """Place instance from place model"""
+    yield Place.objects.create(name='TestPlace')
+
+
+@pytest.fixture(scope='function')
+def camping(user, place):
+    """Camping instance from camping model"""
+    yield Camping.objects.create(name='Test', owner=user, place=place)
+
