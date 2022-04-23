802e131bc6041081bd9cc4068eb01aa9ab288101
wnumik <k1wsatz@wnumiks-MacBook-Pro.local>
2022-04-23 15:20:10 +0200

    docker & django bump

--- users/tests/test_views.py
+++
@@ -1,9 +0,0 @@
-from django.urls import reverse
-
-
-def test_login_page(client):
-    url = reverse('users:login')
-    response = client.get(url)
-
-    assert response.status_code == 200
-    assert '<h1>Login</h1>' in response.content.decode('UTF-8')
