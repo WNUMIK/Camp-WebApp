c6b38c19cd22c740467914fae5d9e19775d18ab8
wnumik <k1wsatz@wnumiks-MacBook-Pro.local>
2022-04-23 23:51:25 +0200

    heroku media

--- config/settings.py
+++ config/settings.py
@@ -142,7 +142,7 @@ STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.AppDirectoriesFinder'
 ]
 
-MEDIA_URL = 'media/'
+MEDIA_URL = 'http://s3.amazonaws.com/campbucket/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
