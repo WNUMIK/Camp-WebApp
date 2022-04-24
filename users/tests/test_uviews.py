6a949dd97feba9e32e9f72c669bcbb150eed9035
wnumik <k1wsatz@wnumiks-MacBook-Pro.local>
2022-04-24 11:57:40 +0200

    test update

--- users/tests/test_uviews.py
+++ users/tests/test_uviews.py
@@ -26,28 +26,3 @@ def test_registration_user(db, client):
 
 def test_login(user, client):
     assert client.login(email='a@a.pl', password='testPass123')
-
-
-# @pytest.mark.parametrize('email, fullname, password1, password2, status_code, number', (
-#         ('tester@tester.pl', 'tester', 'ExamplePass', 'ExamplePass', 200, 1),
-#         # ('', 'janusz', 'ExamplePass', 'ExamplePass', 200, 0),
-#         # ('a', 'januszek', 'ExamplePass4', 'ExamplePass4', 200, 0),
-#         # ('tester1', 'dodo', 'ExamplePass1', 'ExamplePass1', 200, 1),
-#         # ('tester', 'diax', 'password', 'password', 200, 0),
-#         # ('tester2', 'wow', 'ExamplePass1', 'ExamplePass', 200, 0),
-#         # ('tester', 'Mirek', 'ExamplePass', 'ExamplePass2', 200, 0),
-#         # ('tester4', 'Wow', '', 'ExamplePass2', 200, 0),
-#         # ('tester4', 'Jasio', 'ExamplePass2', '', 200, 0),
-#         # ('tester4', '', '', '', 200, 0),
-#         ('', '', '', '', 200, 0),
-# ))
-# def test_registration_user2(db, client, email, fullname, password1, password2, status_code, number):
-#     response = client.post(reverse('users:registration'), data={
-#         'email': email,
-#         'fullname': fullname,
-#         'password1': password1,
-#         'password2': password2,
-#     })
-#
-#     assert response.status_code == status_code
-#     assert get_user_model().objects.all().count() == number
